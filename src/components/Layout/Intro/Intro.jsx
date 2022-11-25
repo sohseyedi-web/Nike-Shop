5d4dfe6f3d0ede8166639ef18070b2f0cf1be519
sohseyedi <sohseyedi@gmail.com>
2022-11-25 17:20:11 +0330

    create intro

--- src/components/Layout/Intro/Intro.jsx
+++ src/components/Layout/Intro/Intro.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import "./Intro.scss";
+import Nike from "../../../assets/hero.png";
+import {FiGithub  ,FiInstagram , FiTwitter , FiLinkedin  ,FiMail} from 'react-icons/fi'
 
 const Intro = () => {
   return (
-    <div>Intro</div>
-  )
-}
+    <header className="header">
+      <div className="header-container container">
+        <h3 className="header-container__title">
+          با کفش های نایک
+          <br />
+          مدل 2.0 بدون مشکل ورزش کن
+        </h3>
+        <button className="header-container__btn">جستجوی محصولات</button>
+        <img className="header-container__img" src={Nike} alt="" />
+        <div className="header-container__social">
+          <span><FiGithub/></span>
+          <span><FiInstagram/></span>
+          <span><FiLinkedin/></span>
+          <span><FiTwitter/></span>
+          <span><FiMail/></span>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Intro
+export default Intro;
--- src/container/Navbar/Navbar.jsx
+++ src/container/Navbar/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Logo from "../../assets/logo.png";
+import './Navbar.scss'
+import {
+  RiSearchLine,
+  RiHeart3Line,
+  RiShoppingBasketLine,
+} from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navs">
+      <div className="container navs-container">
+        <img src={Logo} alt="" className="navs-container__image"/>
+        <div className="navs-container__actions">
+          <div className="navs-container__actions-icon">
+            <RiSearchLine size={28} />
+          </div>
+          <div className="navs-container__actions-icon">
+            <RiHeart3Line size={28} />
+          </div>
+          <div className="navs-container__actions-icon">
+            <span>
+              <RiShoppingBasketLine size={28} />
+            </span>
+            <span className="navs-container__actions-icon__num">0</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
